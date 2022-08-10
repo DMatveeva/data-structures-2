bdb3d026ca69d8b538d51f57524bde9696876e9b
Darya Matveeva <darya.d.chernikova@gmail.com>
2022-08-11 00:43:36 +0300

    balanced bst

---
+++ BalancedBST.py
@@ -0,0 +1,80 @@
+class BSTNode:
+
+    def __init__(self, key, parent):
+        self.NodeKey = key
+        self.Parent = parent
+        self.LeftChild = None
+        self.RightChild = None
+        self.Level = 0
+
+
+class BalancedBST:
+
+    def __init__(self):
+        self.Root = None  # корень дерева
+
+    def GenerateTree(self, a):
+        a.sort()
+        self.generate_recursive(None, a, 0, len(a))
+
+    def generate_recursive(self, parent_node, a, start_index, end_index):
+        index = (end_index - start_index) // 2
+        key = a[index]
+        node = BSTNode(key, parent_node)
+        if parent_node is None:
+            self.Root = node
+            node.Level = 0
+        elif parent_node.NodeKey > key:
+            parent_node.LeftChild = node
+            node.Level = parent_node.Level + 1
+        else:
+            parent_node.RightChild = node
+            node.Level = parent_node.Level + 1
+
+        self.generate_recursive(node, a, start_index, index - 1)
+        self.generate_recursive(node, a, index + 1, end_index)
+
+        return node
+
+    def IsBalanced(self, root_node):
+        return self.is_node_balanced(root_node)
+
+    def is_node_balanced(self, node):
+        if node is None:
+            return True
+
+        if not node.LeftChild and not node.RightChild:
+            return True
+        if not node.LeftChild:
+            self.is_node_balanced(node.RightChild)
+        if not node.RightChild:
+            self.is_node_balanced(node.LeftChild)
+
+        self.is_node_balanced(node.LeftChild)
+        self.is_node_balanced(node.RightChild)
+
+        left_depth = self.get_depth(node.LeftChild)
+        right_depth = self.get_depth(node.RightChild)
+
+        if abs(left_depth - right_depth) > 1:
+            return False
+        return True
+
+    def get_depth(self, node):
+
+        if node.LeftChild is None and node.RightChild is None:
+            return node.Level
+        if node.LeftChild:
+            return self.get_depth(node.LeftChild)
+        if node.RightChild:
+            return self.get_depth(node.RightChild)
+
+        left_depth = self.get_depth(node.LeftChild)
+        right_depth = self.get_depth(node.LeftChild)
+
+        if left_depth > right_depth:
+            return left_depth
+        else:
+            return right_depth
+
+
