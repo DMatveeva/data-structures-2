10ac4afb9fd8dbfad1aa86faee9aecfa89b11989
Darya Matveeva <darya.d.chernikova@gmail.com>
2022-08-20 10:09:50 +0300

    add vertex

---
+++ SimpleGraph.py
@@ -0,0 +1,58 @@
+class Vertex:
+
+    def __init__(self, val):
+        self.Value = val
+
+
+class SimpleGraph:
+
+    def __init__(self, size):
+        self.max_vertex = size
+        self.m_adjacency = [[0] * size for _ in range(size)]
+        self.vertex = [None] * size
+
+    def AddVertex(self, v):
+        new_vertex = Vertex(v)
+        new_index = 0
+        while new_index < self.max_vertex:
+            if not self.vertex[new_index]:
+                break
+            new_index += 1
+        self.vertex[new_index] = new_vertex
+        j = 0
+        while j <= new_index:
+            self.m_adjacency[j][new_index] = 0
+            self.m_adjacency[new_index][j] = 0
+            j += 1
+
+    def RemoveVertex(self, v):
+        i = v
+        while i < self.max_vertex - 1:
+            self.vertex[i] = self.vertex[i + 1]
+        self.vertex[self.max_vertex - 1] = None
+        x = 0
+
+        while x < self.max_vertex - 1:
+            y = v + 1
+            while y < self.max_vertex - 1:
+                self.m_adjacency[y][x] = self.m_adjacency[y + 1][x]
+            x += 1
+
+        y = 0
+        while y < self.max_vertex - 1:
+            x = v + 1
+            while x < self.max_vertex - 1:
+                self.m_adjacency[y][x] = self.m_adjacency[y][x + 1]
+            y += 1
+
+    def IsEdge(self, v1, v2):
+        return True if self.m_adjacency[v1][v2] == 1 else False
+
+
+    def AddEdge(self, v1, v2):
+        self.m_adjacency[v1][v2] = 1
+        self.m_adjacency[v2][v1] = 1
+
+    def RemoveEdge(self, v1, v2):
+        self.m_adjacency[v1][v2] = 0
+        self.m_adjacency[v2][v1] = 0
---
+++ tests/SimpleGraphTest.py
@@ -0,0 +1,36 @@
+import unittest
+
+from SimpleGraph import SimpleGraph
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_1(self):
+        graph = SimpleGraph(3)
+        graph.AddVertex(0)
+        expected_vertex = '0,n,n,'
+        self.assertEqual(expected_vertex, self.vertex_to_str(graph))  # add assertion here
+        expected_m = '[0, 0, 0]\n' \
+                     '[0, 0, 0]\n' \
+                     '[0, 0, 0]\n'
+        self.assertEqual(expected_m, self.m_adjacency_to_str(graph))  # add assertion here
+
+
+    def vertex_to_str(self, graph):
+        s = ''
+        for v in graph.vertex:
+            value = str(v.Value) if v else 'n'
+            s += value + ','
+        return s
+
+    def m_adjacency_to_str(self, graph):
+        s = ''
+        for line in graph.m_adjacency:
+            s += str(line) + '\n'
+        return s
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
